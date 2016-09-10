e63b79becd29e9e183a8133557b9a3da90d50840
Michael Beckwith <tw2113@gmail.com>
2016-09-09 23:04:52 -0500

    brackets, spaces to tabs

--- includes/bp-media-activity.php
+++ includes/bp-media-activity.php
@@ -5,10 +5,12 @@
  */
 function bp_media_activity_button() {
 
-	if( bp_is_group() && !groups_get_groupmeta( bp_get_group_id(), 'enable_media' ) && !defined('ENABLE_MEDIA') ) return;
+	if( bp_is_group() && !groups_get_groupmeta( bp_get_group_id(), 'enable_media' ) && !defined('ENABLE_MEDIA') ) {
+		return;
+	}
 
 	echo '<div id="plupload-upload-ui">';
-		echo '<input id="plupload-browse-button" class="activity-attach-button" type="button" value="' . esc_attr__("Add Image") . '" class="button" />';	
+		echo '<input id="plupload-browse-button" class="activity-attach-button" type="button" value="' . esc_attr__("Add Image") . '" class="button" />';
 	echo '</div>';
 	echo '<input type="hidden" id="bp-media-attachment-id" name="bp-media-attachment-id" value=""/>';
 	echo '<input type="hidden" id="bp-media-user-id" name="bp-media-user-id" value="'. bp_loggedin_user_id() .'"/>';
@@ -44,9 +46,10 @@ function bp_media_register_template_location() {
  */
 function bp_media_replace_get_post_form_template( $templates, $slug, $name ) {
 
-	 if( 'activity/post-form' != $slug )
-        return $templates;
-         
+	 if ( 'activity/post-form' != $slug ) {
+		 return $templates;
+	 }
+
     return array( 'media/post-form.php' );
 }
 
@@ -55,11 +58,12 @@ function bp_media_replace_get_post_form_template( $templates, $slug, $name ) {
  * The bp_media_replace_post_form function.
  */
 function bp_media_replace_post_form() {
-     
-    if( function_exists( 'bp_register_template_stack' ) )
-        bp_register_template_stack( 'bp_media_register_template_location' );
-     
+
+    if ( function_exists( 'bp_register_template_stack' ) ) {
+	    bp_register_template_stack( 'bp_media_register_template_location' );
+    }
+
     add_filter( 'bp_get_template_part', 'bp_media_replace_get_post_form_template', 10, 3 );
-     
+
 }
 add_action( 'bp_init', 'bp_media_replace_post_form' );
