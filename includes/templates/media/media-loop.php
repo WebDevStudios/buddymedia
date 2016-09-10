bd727d153773c035a6ed1af1e0b01d584ad3baa4
Michael Beckwith <tw2113@gmail.com>
2016-09-09 23:41:42 -0500

    code cleanup and blank lines

--- includes/templates/media/media-loop.php
+++ includes/templates/media/media-loop.php
@@ -3,31 +3,31 @@
 <div class="media no-ajax" role="main">
 
 	<ul id="media-stream" class="media-list grid-list">
-	
-		<?php if( bp_media_can_edit() ) : ?>
+
+		<?php if ( bp_media_can_edit() ) : ?>
 			<li class="<?php bp_media_css_class(); ?> create-album" id="media-<?php bp_media_css_id(); ?>">
-			
+
 				<a href="<?php bp_media_create_album_link() ; ?>">
-					
+
 					<div class="album-title"><?php _e( '+ Create Album', 'bp_album'); ?></div>
-					
+
 				</a>
-				
+
 			</li>
 		<?php endif ; ?>
-		
+
 		<?php if ( $query->have_posts() ) : ?>
-			
+
 			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-	
+
 				<?php bp_media_get_template_part( 'album' ); ?>
-	
+
 			<?php endwhile; ?>
-			
+
 		<?php endif; ?>
-	
+
 	</ul>
-	
+
 	<div id="pop"></div>
 </div>
 
