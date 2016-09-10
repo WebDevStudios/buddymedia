e442fdee99d2d3471c56fd5d81ce418a8a97e6f5
Michael Beckwith <tw2113@gmail.com>
2016-09-09 23:39:23 -0500

    phpdoc cleanup

--- includes/bp-media-library-filter.php
+++ includes/bp-media-library-filter.php
@@ -2,19 +2,21 @@
 
 class BP_MEDIA_LIBRARY_FILTER {
 	/**
-	 * Defines media types
+	 * Defines media types/
+	 *
 	 * @var array
 	 */
 	protected static $media_types = array();
 
 	/**
-	 * Holds a singleton instance of this class
+	 * Holds a singleton instance of this class.
+	 *
 	 * @var null
 	 */
 	protected static $instance = null;
 
 	/**
-	 * Returns an instance of this class
+	 * Returns an instance of this class.
 	 */
 	public static function get_instance() {
 		if ( null === self::$instance ) {
@@ -29,7 +31,6 @@ class BP_MEDIA_LIBRARY_FILTER {
 	 * __construct function.
 	 *
 	 * @access private
-	 * @return void
 	 */
 	private function __construct() {
 		add_action( 'pre_get_posts' , array( $this, 'bp_media_pre_get_posts' ) );
@@ -37,18 +38,19 @@ class BP_MEDIA_LIBRARY_FILTER {
 	}
 
 	/**
-	 * bp_media_pre_get_posts function.
+	 * The bp_media_pre_get_posts function.
 	 *
 	 * @access public
-	 * @param mixed $query
-	 * @return void
+	 *
+	 * @param WP_Query $query Query object.
+	 * @return WP_Post $query
 	 */
 	public function bp_media_pre_get_posts( $query ) {
 		global $pagenow;
 
 		if( ( 'edit.php' != $pagenow && 'upload.php' != $pagenow   ) || !$query->is_admin && $query->is_main_query() ) return $query;
 
-		// filter out user media by default
+		// Filter out user media by default.
 		$query->set( 'meta_query', array(
 				array(
 					'key' => 'bp_media',
@@ -64,14 +66,14 @@ class BP_MEDIA_LIBRARY_FILTER {
 
 	
 	/**
-	 * bp_media_ajax_pre_get_posts function.
-	 * 
+	 * The bp_media_ajax_pre_get_posts function.
+	 *
 	 * @access public
-	 * @param array $query (default: array())
-	 * @return void
+	 * @param array $query (default: array()).
+	 * @return array
 	 */
 	public function bp_media_ajax_pre_get_posts( $query = array() ) {
-			// filter out user media by default
+		// Filter out user media by default.
 		$query['meta_query'] = array(
 				array(
 					'key' => 'bp_media',
@@ -80,6 +82,6 @@ class BP_MEDIA_LIBRARY_FILTER {
 			);
 	    return $query;
 	}
-	
+
 }
 BP_MEDIA_LIBRARY_FILTER::get_instance();
