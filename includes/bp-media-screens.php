7a3d95e7948b4530e222cf58fca47838db7535cb
Michael Beckwith <tw2113@gmail.com>
2016-09-09 23:30:19 -0500

    phpdoc cleanup

--- includes/bp-media-screens.php
+++ includes/bp-media-screens.php
@@ -15,9 +15,8 @@ class BP_Media_Theme_Compat {
 
     /**
      * __construct function.
-     * 
+     *
      * @access public
-     * @return void
      */
     public function __construct() {
 
@@ -27,8 +26,8 @@ class BP_Media_Theme_Compat {
 
 
     /**
-     * setup_actions function.
-     * 
+     * The setup_actions function.
+     *
      * @access public
      * @return void
      */
@@ -44,13 +43,12 @@ class BP_Media_Theme_Compat {
 
 
 	/**
-	 * media_screen_index function.
-	 * 
+	 * The media_screen_index function.
+	 *
 	 * @access public
-	 * @return void
 	 */
 	public function media_screen_index() {
-	    // check if on media directory...
+	    // Check if on media directory.
 	    if ( !bp_displayed_user_id() && bp_is_current_component( 'media' ) && !bp_current_action() ) {
 
 	        bp_update_is_directory( true, 'media' );
@@ -61,14 +59,14 @@ class BP_Media_Theme_Compat {
 
 
 	/**
-	 * template_hierarchy function.
-	 * 
+	 * The template_hierarchy function.
+	 *
 	 * @access public
-	 * @param mixed $templates
+	 * @param mixed $templates Templates array.
 	 * @return array $templates Array of custom templates.
 	 */
 	public function template_hierarchy( $templates ) {
-	    // if on a page of  plugin, then we add our path to the template path array
+	    // If on a page of  plugin, then we add our path to the template path array.
 	    if ( bp_is_current_component( 'media' ) ) {
 
 	        $templates[] = BP_MEDIA_PLUGIN_DIR . '/includes/templates';
@@ -105,12 +103,11 @@ class BP_Media_Theme_Compat {
 
 
     /**
-     * directory_dummy_post function.
+     * The directory_dummy_post function.
      *
      * Update the global $post with directory data
-     * 
+     *
      * @access public
-     * @return void
      */
     public function directory_dummy_post() {
 
@@ -129,10 +126,9 @@ class BP_Media_Theme_Compat {
 
 
     /**
-     * directory_content function.
-     * 
+     * The directory_content function.
+     *
      * @access public
-     * @return void
      */
     public function directory_content() {
         bp_buffer_template_part( 'media/directory-index');
@@ -144,10 +140,9 @@ new BP_Media_Theme_Compat();
 
 
 /**
- * bp_media_get_templates_dir function.
- * 
+ * The bp_media_get_templates_dir function.
+ *
  * @access public
- * @return void
  */
 function bp_media_get_templates_dir() {
 	return BP_MEDIA_PLUGIN_DIR . 'includes/templates/media';
@@ -155,46 +150,46 @@ function bp_media_get_templates_dir() {
 
 
 /**
- * bp_media_get_template_part function.
- * 
+ * The bp_media_get_template_part function.
+ *
  * @access public
- * @param mixed $slug
- * @param mixed $name (default: null)
- * @param bool $load (default: true)
- * @return void
+ * @param mixed $slug Template slug.
+ * @param mixed $name Default: null.
+ * @param bool  $load Default: true.
+ * @return string
  */
 function bp_media_get_template_part( $slug, $name = null, $load = true ) {
-	// Execute code for this part
+	// Execute code for this part.
 	do_action( 'get_template_part_' . $slug, $slug, $name );
- 
-	// Setup possible parts
+
+	// Setup possible parts.
 	$templates = array();
 	if ( isset( $name ) )
 		$templates[] = $slug . '-' . $name . '.php';
 	$templates[] = $slug . '.php';
- 
-	// Allow template parts to be filtered
+
+	// Allow template parts to be filtered.
 	$templates = apply_filters( 'bp_media_get_template_part', $templates, $slug, $name );
- 
-	// Return the part that is found
+
+	// Return the part that is found.
 	return bp_media_locate_template( $templates, $load, false );
 }
 
 
 /**
- * bp_media_locate_template function.
- * 
+ * The bp_media_locate_template function.
+ *
  * @access public
- * @param mixed $template_names
- * @param bool $load (default: false)
- * @param bool $require_once (default: true)
- * @return void
+ * @param mixed $template_names Template names.
+ * @param bool  $load           Default: false.
+ * @param bool  $require_once   Default: true.
+ * @return string
  */
 function bp_media_locate_template( $template_names, $load = false, $require_once = true ) {
-	// No file found yet
+	// No file found yet.
 	$located = false;
- 
-	// Try to find a template file
+
+	// Try to find a template file.
 	foreach ( (array) $template_names as $template_name ) {
  
 		// Continue if template is empty
@@ -203,18 +198,18 @@ function bp_media_locate_template( $template_names, $load = false, $require_once
 			
 		// Trim off any slashes from the template name
 		$template_name = ltrim( $template_name, '/' );
- 
-		// Check child theme first
+
+		// Check child theme first.
 		if ( file_exists( trailingslashit( get_stylesheet_directory() ) . 'media/' . $template_name ) ) {
 			$located = trailingslashit( get_stylesheet_directory() ) . 'media/' . $template_name;
 			break;
- 
-		// Check parent theme next
+
+		// Check parent theme next.
 		} elseif ( file_exists( trailingslashit( get_template_directory() ) . 'media/' . $template_name ) ) {
 			$located = trailingslashit( get_template_directory() ) . 'media/' . $template_name;
 			break;
- 
-		// Check theme compatibility last
+
+		// Check theme compatibility last.
 		} elseif ( file_exists( trailingslashit( bp_media_get_templates_dir() ) . $template_name ) ) {
 			$located = trailingslashit( bp_media_get_templates_dir() ) . $template_name;
 			break;
@@ -229,8 +224,8 @@ function bp_media_locate_template( $template_names, $load = false, $require_once
 
 
 /**
- * bp_media_screen_my_media function.
- * 
+ * The bp_media_screen_my_media function.
+ *
  * @access public
  * @return void
  */
@@ -247,8 +242,8 @@ function bp_media_screen_user_media() {
 
 
 /**
- * bp_media_gallery_content function.
- * 
+ * The bp_media_gallery_content function.
+ *
  * @access public
  * @return void
  */
