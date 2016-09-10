eca48533b00eee32fdcdde0cec09fd2fee6b4c2b
Michael Beckwith <tw2113@gmail.com>
2016-09-09 23:13:01 -0500

    equal sign alignment

--- includes/bp-media-ajax.php
+++ includes/bp-media-ajax.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * The bp_media_upload_photo function.
@@ -6,23 +6,23 @@
 function bp_media_upload_photo() {
 
 	check_ajax_referer('photo-upload');
-	
-	// upload file
-	$file = $_FILES['async-upload'];
+
+	// Upload file.
+	$file   = $_FILES['async-upload'];
 	$status = wp_handle_upload( $file, array( 'test_form' => true, 'action' => 'photo_gallery_upload' ) );
 	
 	$wp_upload_dir = wp_upload_dir();
 
 	// Adds file as attachment to WordPress.
 	$attachment = array(
-		'guid'           	=> $wp_upload_dir['url'] . '/' . basename( $status['file'] ), 
-		'post_mime_type' 	=> $status['type'],
-		'post_title' 		=> preg_replace( '/\.[^.]+$/', '', basename( $status['file'] ) ),
-		'post_content' 		=> '',
-		'post_status' 		=> 'inherit'
+		'guid'           => $wp_upload_dir['url'] . '/' . basename( $status['file'] ),
+		'post_mime_type' => $status['type'],
+		'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $status['file'] ) ),
+		'post_content'   => '',
+		'post_status'    => 'inherit'
 	);
-	$attach_id = wp_insert_attachment( $attachment, $status['file'], (int) $_POST['gallery_id'] );
-	
+	$attach_id  = wp_insert_attachment( $attachment, $status['file'], (int) $_POST['gallery_id'] );
+
 	// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
 	require_once( ABSPATH . 'wp-admin/includes/image.php' );
 	
@@ -36,8 +36,8 @@ function bp_media_upload_photo() {
 	$image = wp_get_attachment_image_src( $attach_id, 'thumbnail');
 	
 	$data = array(
-		'id' => $attach_id,
-		'url' => $image[0]
+		'id'  => $attach_id,
+		'url' => $image[0],
 	);
 	
 	wp_send_json( $data );
@@ -64,11 +64,11 @@ function bp_media_photo_activity_attach() {
 
 	// Adds file as attachment to WordPress.
 	$attachment = array(
-		'guid'           	=> $wp_upload_dir['url'] . '/' . basename( $status['file'] ), 
-		'post_mime_type' 	=> $status['type'],
-		'post_title' 		=> preg_replace( '/\.[^.]+$/', '', basename( $status['file'] ) ),
-		'post_content' 		=> '',
-		'post_status' 		=> 'inherit'
+		'guid'           => $wp_upload_dir['url'] . '/' . basename( $status['file'] ),
+		'post_mime_type' => $status['type'],
+		'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $status['file'] ) ),
+		'post_content'   => '',
+		'post_status'    => 'inherit'
 	);
 	$attach_id = wp_insert_attachment( $attachment, $status['file'], (int) $album_id );
 	
@@ -85,9 +85,9 @@ function bp_media_photo_activity_attach() {
 	$image = wp_get_attachment_image_src( $attach_id, 'thumbnail');
 	
 	$data = array(
-		'id' => $attach_id,
+		'id'       => $attach_id,
 		'album_id' => $album_id,
-		'url' => $image[0]
+		'url'      => $image[0],
 	);
 	
 	wp_send_json( $data );
@@ -109,20 +109,20 @@ function bp_media_get_activity_album_id( $user_id ) {
 	if( ! $user_id ) return;
 
 	$post = get_posts( array(
-	    'meta_key'   => '_activity_album',
-	    'author' => (int) $user_id,
-	    'post_type' => 'bp_media'
+	    'meta_key'  => '_activity_album',
+	    'author'    => (int) $user_id,
+	    'post_type' => 'bp_media',
 	) );
 	
 	if( $post ) return $post[0]->ID;
 
 	// Create post object
 	$my_post = array(
-	  'post_title'    => __( 'Activity Attachments', 'bp-media' ),
-	  'post_content'  => __( 'Images upload while posting activity.', 'bp-media' ),
-	  'post_status'   => 'publish',
-	  'post_author'   => (int) $user_id,
-	  'post_type' => 'bp_media'
+	  'post_title'   => __( 'Activity Attachments', 'bp-media' ),
+	  'post_content' => __( 'Images upload while posting activity.', 'bp-media' ),
+	  'post_status'  => 'publish',
+	  'post_author'  => (int) $user_id,
+	  'post_type'    => 'bp_media',
 	);
 	
 	add_filter('bp_activity_type_before_save', '__return_false', 9999 );
@@ -141,15 +141,14 @@ function bp_media_get_activity_album_id( $user_id ) {
  * The bp_media_get_image function.
  */
 function bp_media_get_image(){
-	
-	$photo_id =  $_GET['id'];
-	$guid =  $_GET['guid'];
-	$user_id =  $_GET['user'];
-	
-	$user = get_user_by( 'id', (int) $user_id );
-	
+
+	$photo_id = $_GET['id'];
+	$guid     = $_GET['guid'];
+	$user_id  = $_GET['user'];
+	$user     = get_user_by( 'id', (int) $user_id );
+
 	include( bp_media_get_template_part( 'single/image') );
-	
+
 	die();
 }
 add_action('wp_ajax_bp_media_get_image', 'bp_media_get_image');
@@ -178,18 +177,18 @@ function bp_media_ajax_create_album(){
 
 	check_ajax_referer( 'create-album', 'nonce' );
 
-	$title =  $_GET['title'];
-	$content =  $_GET['description'];
-	$permission =  ( !empty($_GET['permission']) ) ? $_GET['permission'] : 'public' ;
-	$user_id =  $_GET['user_id'];
+	$title      = $_GET['title'];
+	$content    = $_GET['description'];
+	$permission = ( ! empty( $_GET['permission'] ) ) ? $_GET['permission'] : 'public';
+	$user_id    = $_GET['user_id'];
 
 	// Create post object.
 	$my_post = array(
-	  'post_title'    => sanitize_text_field( $title ),
-	  'post_content'  => sanitize_text_field( $content ),
-	  'post_status'   => 'publish',
-	  'post_author'   => (int) $user_id,
-	  'post_type' => 'bp_media'
+		'post_title'   => sanitize_text_field( $title ),
+		'post_content' => sanitize_text_field( $content ),
+		'post_status'  => 'publish',
+		'post_author'  => (int) $user_id,
+		'post_type'    => 'bp_media',
 	);
 	
 	// dont post activity if album is not public
@@ -225,17 +224,17 @@ function bp_media_ajax_edit_album(){
 
 	check_ajax_referer( 'edit-album', 'nonce' );
 
-	$title =  $_GET['title'];
-	$content =  $_GET['description'];
-	$user_id =  $_GET['user_id'];
-	$post_id =  $_GET['post_id'];
-	$permission =  $_GET['permission'];
-	
-	// Update post
+	$title      = $_GET['title'];
+	$content    = $_GET['description'];
+	$user_id    = $_GET['user_id'];
+	$post_id    = $_GET['post_id'];
+	$permission = $_GET['permission'];
+
+	// Update post.
 	$my_post = array(
 	  'ID'           => (int) $post_id,
-	  'post_title'    => sanitize_text_field( $title ),
-	  'post_content'  => sanitize_text_field( $content ),
+	  'post_title'   => sanitize_text_field( $title ),
+	  'post_content' => sanitize_text_field( $content ),
 	);
 
 	// Update the post into the database.
@@ -290,12 +289,11 @@ function bp_media_ajax_delete_image(){
 
 	check_ajax_referer( 'edit-album', 'nonce' );
 
-	$user_id =  ( !empty( $_GET['user_id'] ) ) ?  $_GET['user_id'] : get_current_user_id();
-	$image_id =  $_GET['image_id'];
-	
-	$parent = get_post_field( 'post_parent', $image_id);
-	
-	if( $activity_id = get_post_meta( $image_id, 'activity_id', true ) ) {
+	$user_id  = ( ! empty( $_GET['user_id'] ) ) ? $_GET['user_id'] : get_current_user_id();
+	$image_id = $_GET['image_id'];
+	$parent   = get_post_field( 'post_parent', $image_id );
+
+	if ( $activity_id = get_post_meta( $image_id, 'activity_id', true ) ) {
 		bp_activity_delete( array( 'id' => $activity_id ) );
 	}
 
@@ -304,8 +302,8 @@ function bp_media_ajax_delete_image(){
 
 	// Return post id.
 	$data = array(
-		'id' =>  $image_id,
-		'url' => bp_core_get_user_domain( $user_id ) . BP_MEDIA_SLUG . '/album/' . $parent
+		'id'  => $image_id,
+		'url' => bp_core_get_user_domain( $user_id ) . BP_MEDIA_SLUG . '/album/' . $parent,
 	);
 	
 	wp_send_json( $data );
@@ -322,16 +320,16 @@ function bp_media_ajax_edit_image(){
 
 	check_ajax_referer( 'edit-album', 'nonce' );
 
-	$user_id =  $_GET['user_id'];
-	$image_id =  $_GET['image_id'];
-	$description =  sanitize_text_field( $_GET['description'] );
-		
-	// delete the post
+	$user_id     = $_GET['user_id'];
+	$image_id    = $_GET['image_id'];
+	$description = sanitize_text_field( $_GET['description'] );
+
+	// Delete the post.
 	update_post_meta( (int) $image_id, 'description', $description );
 		
 	$data = array(
-		'id' =>  $image_id,
-		'url' =>  bp_core_get_user_domain( $user_id ) . BP_MEDIA_SLUG . '/image/' . $image_id
+		'id'  => $image_id,
+		'url' => bp_core_get_user_domain( $user_id ) . BP_MEDIA_SLUG . '/image/' . $image_id,
 	);
 	
 	wp_send_json( $data );
@@ -357,18 +355,18 @@ function bp_media_ajax_add_comment(){
 	$time = current_time('mysql');
 	
 	$data = array(
-	    'comment_post_ID' => $post_id,
-	    'comment_author' => '',
+	    'comment_post_ID'      => $post_id,
+	    'comment_author'       => '',
 	    'comment_author_email' => '',
-	    'comment_author_url' => '',
-	    'comment_content' => $comment,
-	    'comment_type' => '',
-	    'comment_parent' => 0,
-	    'user_id' => $user_id,
-	    'comment_author_IP' => '',
-	    'comment_agent' => '',
-	    'comment_date' => $time,
-	    'comment_approved' => 1,
+	    'comment_author_url'   => '',
+	    'comment_content'      => $comment,
+	    'comment_type'         => '',
+	    'comment_parent'       => 0,
+	    'user_id'              => $user_id,
+	    'comment_author_IP'    => '',
+	    'comment_agent'        => '',
+	    'comment_date'         => $time,
+	    'comment_approved'     => 1,
 	);
 	
 	$data = apply_filters( 'bp_media_ajax_add_comment', $data );
